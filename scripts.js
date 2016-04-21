49bb3d8c31af5c8012ffad06d04900b14f5339f5
aleksandrpetrunin <petrunin.aleksandr@gmail.com>
2016-04-21 21:19:20 +0300

    Added files via upload
    Additional Js script files managing context via .html  & .txt

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@
 
 $('<input type="button" value="toggle" id="toggleButton">').insertAfter('#disclaimer');
 
+//Adding Buttom Before div...
+//$('<input type="button" value="toggle" //id="toggleButton">').insertBefore('#disclaimer');
+
 //Programming logics
 
 $('#toggleButton').click(function(){
@@ -10,3 +13,23 @@ $('#toggleButton').click(function(){
     if ($('#disclaimer').is(':visible')) {$(this).val('Hide');}else{$(this).val('Show');}
     
 });
+
+//inserting elements inside the element
+$('<strong>START!<strong/>').prependTo('#disclaimer');
+$('<strong>STOP!<strong/>').appendTo('#disclaimer');
+
+//Short-hand syntax Does not work
+//$('<div>'.{
+//    id:'specialButton',
+//    text:'Click Me!',
+//    click: function(){
+//    alert("Advanced jQuery!")
+//}
+//}).insertBefore('#disclaimer');
+
+//Context Modification
+$('p').html('<strong>Warning!<strong/>Text has been modifed');
+$('h2').text('Title elements has been modified');
+var userName =prompt('Please, enter you name..');
+$('#disclaimer').text('Welcome '+userName);
+
